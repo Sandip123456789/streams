abec6d83476679625c645049931af907888282a2
Sandip Chaudhary <sndp.c114@gmail.com>
2021-06-30 18:45:40 +0545

    Updated as well as refactored some components and also added reducers and modal

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
 import StreamEdit from './streams/StreamEdit';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import Header from './Header';
+import history from '../history';
 
 //#41B3A3
 const App = () => {
@@ -14,14 +15,14 @@ const App = () => {
   });
   return (
     <div className="ui container">
-      <Router>
+      <Router history={history}>
         <div>
           <Header />
           <Route path="/" exact component={StreamList} />
-          <Route path="/streams/show" component={StreamShow} />
-          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/:id" exact component={StreamShow} />
+          <Route path="/streams/edit/:id" exact component={StreamEdit} />
           <Route path="/streams/new" component={StreamCreate} />
-          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/delete/:id" component={StreamDelete} />
         </div>
       </Router>
     </div>
